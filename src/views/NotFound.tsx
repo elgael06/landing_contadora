154c8d58e76a11dbcb905d67b96014459dd5b64c
elgael06 <elgael06@gmail.com>
2021-12-12 20:00:03 -0700

    message page not found

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,16 +1,17 @@
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import themeSignal from "../signal/theme.signal";
 
 
 const NotFoundPage = () => {
+    let location = useLocation();
     useEffect(()=>{
         themeSignal.setTheme('is-');
     },[]);
     return (<div>
         <article className="notification is-danger">
         <h3 className="title">pagina no encontrada!!!</h3>
-            <p >Error al acceder al directorio.</p>
+            <p >Error al acceder al directorio <code>{location.pathname}</code>.</p>
             <p className="subtitle">
                 <Link to='/'> Regresar a Inicio...</Link> 
             </p>
